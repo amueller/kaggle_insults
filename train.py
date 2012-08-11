30b12da8b73143994fc05b4c65589c70269bb9f2
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-08-11 15:22:52 +0100

    pipeline and big grid search

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.grid_search import GridSearchCV
 #from sklearn.cross_validation import cross_val_score
 #from sklearn.svm import LinearSVC
-#from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 #from scipy import sparse
 #from sklearn.ensemble import RandomForestClassifier
 from IPython.core.debugger import Tracer
@@ -66,10 +66,10 @@ def grid_search():
     comments, dates, labels = load_data()
 
     print("vecorizing")
-    countvect = CountVectorizer(max_n=1)
+    countvect = CountVectorizer(max_n=3)
     #countvect = TfidfVectorizer()
 
-    counts = countvect.fit_transform(comments)
+    #counts = countvect.fit_transform(comments)
     #counts_array = counts.toarray()
     #indicators = sparse.csr_matrix((counts_array > 0).astype(np.int))
 
@@ -78,21 +78,23 @@ def grid_search():
     #inds = np.random.permutation(len(labels))
     #n_samples = len(labels)
     #print("training")
-    #param_grid = dict(C=2. ** np.arange(-4, 1))
-    param_grid = dict(C=2. ** np.arange(-5, 4))
-    #clf = LinearSVC(tol=1e-8, penalty='l2', dual=False, C=0.5)
-    #clf = SVC(tol=1e-8, kernel='linear', probability=True)
+    param_grid = dict(logr__C=2. ** np.arange(-6, 4),
+            logr__penalty=['l1', 'l2'],
+            vect__max_n=np.arange(1, 4), vect__lowercase=[True, False])
+    #clf = LinearSVC(tol=1e-8, penalty='l1', dual=False, C=0.5)
     clf = LogisticRegression(tol=1e-8)
+    pipeline = Pipeline([('vect', countvect), ('logr', clf)])
 
     #clf = NearestCentroid()
 
     #param_grid = dict(max_depth=np.arange(1, 10))
     #clf = RandomForestClassifier(n_estimators=10)
 
-    grid = GridSearchCV(clf, cv=5, param_grid=param_grid, verbose=4)
+    grid = GridSearchCV(pipeline, cv=5, param_grid=param_grid, verbose=4,
+            n_jobs=11)
     #print(cross_val_score(clf, counts, labels, cv=3))
 
-    grid.fit(counts, labels)
+    grid.fit(comments, labels)
     #clf.fit(X_train, y_train)
     #print(clf.score(X_test, y_test))
     comments_test, dates_test = load_test()
